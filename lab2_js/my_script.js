aa1fff09343ad063cb31a4665da3b3afcb9a452a
inf136769 <szymonmichalak97@gmail.com>
2020-03-23 23:00:43 +0100

    fix save and edit

--- lab2_js/my_script.js
+++ lab2_js/my_script.js
@@ -1,7 +1,6 @@
 function add_book(){
-    var tableRef = document.getElementById('myTable').getElementsByTagName('tbody')[0];
-
     // insert a row in the table at the last row
+    var tableRef = document.getElementById('myTable').getElementsByTagName('tbody')[0];
     var newRow   = tableRef.insertRow();
 
     // get amount of rows and set color
@@ -34,42 +33,99 @@ function add_book(){
     button_save.type ="button"
     button_save.id = "save" + rowCount;
     button_save.innerHTML = "Save"
+    button_save.setAttribute('onclick', 'save_function('+ rowCount +')');
     newCell_3.appendChild(button_save);
-    button_save.onclick = function(){
-        // alert(rowCount);
-        var author = input_author.value;
-        var title = input_title.value;
-
-        document.getElementById("author" + rowCount).remove();
-        document.getElementById("title" + rowCount).remove();
-
-        var author_text = document.createTextNode(author)
-        newCell_1.appendChild(author_text);
-        var title_text = document.createTextNode(title)
-        newCell_2.appendChild(title_text);
-
-        button_save.innerHTML = "Edit"
-    }
     
     // configure remove button
     var button_remove = document.createElement("button")
     button_remove.type ="button" 
     button_remove.id = "remove" + rowCount;
     button_remove.innerHTML = "Remove"
+    button_remove.setAttribute('onclick', 'remove_function('+ rowCount +')');
     newCell_3.appendChild(button_remove);
-    button_remove.onclick = function(){
-        tableRef.deleteRow(rowCount);
+    // button_remove.onclick = function(){
+    //     tableRef.deleteRow(rowCount);
 
-        var table = document.getElementById("table_body");
-        for (var i = 0, row; row = table.rows[i]; i++) {
-            row.id = "tr"+i;
-            if(i % 2 == 0){
-                row.className = "gray";
-            }
-            else{
-                row.className = "white"
-            }
-        }
-    }
+    //     var table = document.getElementById("table_body");
+    //     for (var i = 0, row; row = table.rows[i]; i++) {
+    //         row.id = "tr"+i;
+    //         if(i % 2 == 0){
+    //             row.className = "gray";
+    //         }
+    //         else{
+    //             row.className = "white"
+    //         }
+    //         // for(child of row.children){
+    //         //     alert(child.children.id)
+    //         // }
+    //     }
+    // }
     
+}
+
+
+function save_function(row){
+    // get values
+    var title = document.getElementById("title"+row).value;
+    var author = document.getElementById("author"+row).value;
+
+    // remove input fields
+    document.getElementById("author" + row).remove();
+    document.getElementById("title" + row).remove();
+
+    // add text to cell
+    var author_text = document.createTextNode(author);
+    var newCell_1 = document.getElementById("table_body").rows[row].cells[0];
+    newCell_1.appendChild(author_text);
+    var title_text = document.createTextNode(title);
+    var newCell_2 = document.getElementById("table_body").rows[row].cells[1];
+    newCell_2.appendChild(title_text);
+
+    // set button
+    var current_button = document.getElementById("save"+row);
+    current_button.innerHTML = "Edit";
+    current_button.id = "edit"+row;
+    current_button.setAttribute('onclick', 'edit_function('+ row +')');
+}
+
+
+function edit_function(row){
+    var current_author = document.getElementById("table_body").rows[row].cells[0].innerHTML;
+    var current_title = document.getElementById("table_body").rows[row].cells[1].innerHTML;
+
+    // remove text and set input
+    var input_author = document.createElement("input")
+    input_author.type = "text";
+    input_author.id = "author" + row;
+    input_author.value = current_author;
+    document.getElementById("table_body").rows[row].cells[0].innerHTML="";
+    document.getElementById("table_body").rows[row].cells[0].appendChild(input_author);
+
+    var input_title = document.createElement("input")
+    input_title.type = "text";
+    input_title.id = "title" + row;
+    input_title.value = current_title;
+    document.getElementById("table_body").rows[row].cells[1].innerHTML="";
+    document.getElementById("table_body").rows[row].cells[1].appendChild(input_title);
+
+    // set button
+    var current_button = document.getElementById("edit"+row);
+    current_button.innerHTML = "Save";
+    current_button.id = "save"+row;
+    current_button.setAttribute('onclick', 'save_function('+ row +')');
+
+}
+
+function next_save(row){
+    // set button
+    var current_button = document.getElementById("save"+row);
+    current_button.innerHTML = "Edit";
+    current_button.id = "edit"+row;
+    current_button.setAttribute('onclick', 'edit_function('+ row +')');
+}
+
+function remove_function(row){
+    for(let i=0; i<3; i++){
+        document.getElementById("table_body").rows[row].cells[0].remove();
+    }
 }
